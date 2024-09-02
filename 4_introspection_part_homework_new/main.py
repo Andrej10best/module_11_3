a93f624c684b65e8ebbce4de07450e7eec68c44d
Andrej10best <Andrej10best@yandex.ru>
2024-09-02 11:46:19 +0400

    Update main.py

--- 4_introspection_part_homework_new/main.py
+++ 4_introspection_part_homework_new/main.py
@@ -4,7 +4,7 @@ import inspect
 def introspection_info(obj):
     info = {
         'type': type(obj).__name__,
-        'attributes': [attr for attr in dir(obj) if not attr.startswith('__') and not callable(getattr(obj, attr))],
+        'attributes': [attr for attr in dir(obj) if not callable(getattr(obj, attr))],
         'methods': [meth for meth in dir(obj) if meth.startswith('__')],
         'module': inspect.getmodule(introspection_info).__name__,
     }
